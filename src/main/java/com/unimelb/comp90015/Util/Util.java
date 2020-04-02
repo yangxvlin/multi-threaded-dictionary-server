947feeee236458569d812bc3fe27243ebdbcaa2a
yangxvlin <1768528843@qq.com>
2020-04-03 01:42:43 +1100

    add comments

--- src/main/java/com/unimelb/comp90015/Util/Util.java
+++ src/main/java/com/unimelb/comp90015/Util/Util.java
@@ -8,26 +8,47 @@ import static com.unimelb.comp90015.Util.Constant.*;
  * Xulin Yang, 904904
  *
  * @create 2020-03-26 22:23
- * description:
+ * description: some helper functions
  **/
 
 public class Util {
+    /**
+     * @param errorCode    the index of the error
+     * @param errorContent the description of the error
+     * @return formatted error message
+     */
     public static String getError(String errorCode, String errorContent) {
         return "Error(" + errorCode + "): " + errorContent;
     }
 
+    /**
+     * @param vipPriority the vip level
+     * @return true if vipPriority is in 1-10; false otherwise
+     */
     public static boolean checkWrongVipPriority(int vipPriority) {
         return vipPriority < 0 || vipPriority > 10;
     }
 
+    /**
+     * @param serverPort the port number
+     * @return true if port number is in 49152-65535; false otherwise
+     */
     public static boolean checkWrongServerPort(int serverPort) {
         return serverPort < 49152 || serverPort > 65535;
     }
 
+    /**
+     * invoke dialog for port number error
+     */
     public static void serverPortError() {
         popupErrorDialog(ERROR_INVALID_PORT_NUMBER_CODE, ERROR_INVALID_PORT_NUMBER_CONTENT);
     }
 
+    /**
+     * @param code    the index of the error
+     * @param content the description of the error
+     * invoke dialog for error
+     */
     public static void popupErrorDialog(String code, String content) {
         JOptionPane.showConfirmDialog(
                 null,
@@ -38,6 +59,10 @@ public class Util {
         System.exit(1);
     }
 
+    /**
+     * @param size
+     * @return
+     */
     public static boolean checkWrongThreadPoolSize(int size) {
         return size <= 0 || size >= Integer.MAX_VALUE;
     }
